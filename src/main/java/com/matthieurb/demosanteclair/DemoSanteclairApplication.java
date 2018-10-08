8e7db76773ea333a248b0557b9498b67fe72a637
Matthieu Rambaud <rambaud.matthieu@gmail.com>
2018-10-08 18:58:27 +0200

    Initial commit

---
+++ src/main/java/com/matthieurb/demosanteclair/DemoSanteclairApplication.java
@@ -0,0 +1,12 @@
+package com.matthieurb.demosanteclair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSanteclairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSanteclairApplication.class, args);
+	}
+}
